9addd12558bc838e096cfab11ca64d0f0184ede9
ZhuChaozheng <370045744@qq.com>
2018-09-22 19:07:29 +0800

    refine the test client file

--- dogs_client_raspberry_test.py
+++ dogs_client_raspberry_test.py
@@ -19,38 +19,18 @@ def main():
 #  host = '180.109.138.23'
   port = 12343
   s.connect((host, port))
+  # demo data
+  r = ['00', '1e', '6c', '01', 'ea', '44', '00', 'e2', '6c',
+  '01', '1e', '6c', '01', '1e', '6c', '00', '3e', '6c', '00', '7e', '0c']
   while True:
-    str_read = x.read()
-  #  print(str_read)
-    i = ['%02x' % b for b in str_read]
-#print(str_read_byte)
-    print(i)
-    if i[0] == 'aa' and num == 0:
-      num = num + 1
-      continue
-    elif i[0] == 'aa' and num == 1:
-      num = num + 1
-      continue
-    elif i[0] == 'f1' and num == 2:
-      num = num + 1
-      continue
-    if num == 3:
-      length = int(i[0], 16)
-      num = num + 1
-      continue
-    if num > 3 and num < 26:
-      r.extend(i)
-      num = num + 1
-      if len(r) == 21:
-        print('ii', r)
-
+        #print('ii', r)
         a = pickle.dumps(r)
         s.send(a)
         time.sleep(1)
-    if num == 26:
-       num = 0
-       num1 = 0
-       r = []
+    # if num == 26:
+    #    num = 0
+    #    num1 = 0
+    #    r = []
   s.close()
 
 if __name__ == "__main__":
