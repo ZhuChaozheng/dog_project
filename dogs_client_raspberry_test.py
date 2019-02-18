32454cccb879370c927e1d7a017af167c3e114dc
lorthevan <lorthevan@outlook.com>
2019-02-18 20:31:52 +0800

    Revert "multi dog update"
    This reverts commit 7d66a8b8a16fea13f72672508702e5eeb33124f0.

--- dogs_client_raspberry_test.py
+++ dogs_client_raspberry_test.py
@@ -12,10 +12,11 @@ def main():
   s = socket.socket()
 
   # host = 'server.blackant.org'
-  host = 'server.blackant.org'
+#  host = 'server.blackant.org'
 #  host = '180.109.138.23'
-#   host = '127.0.0.1'
-  port = 10011
+  host = '127.0.0.1'
+#  port = 9999
+  port = 12343
   try:
     s.connect((host, port))
     print('connected')
@@ -24,20 +25,19 @@ def main():
   # demo data
 #  r = ['00', '1e', '6c', '01', 'ea', '44', '00', 'e2', '6c',
 #  '01', '1e', '6c', '01', '1e', '6c', '00', '3e', '6c', '00', '7e', '0c']
-  r = ['00', '1e', '6c', '01', 'ea', '44', '00', 'e2', '6c', '6c', '01', 'ea', '44', '00', 'e2', '6c', '6c', '01', 'ea', '44', '00', 'e2', '6c']
+  r = ['00', '1e', '6c', '01', 'ea', '44', '00', 'e2', '6c']
   flag = 0
   while True:
         #print('ii', r)
         if (flag == 0):
-            r = ['01', 'ea', '41', '01', 'ea', '41', '01', 'ea', '41', '01', 'ea', '41', '01', 'ea', '41', '01', 'ea', '41', 'ea', '44', '01', 'ea', '41']
+            r = ['00', '16', '61', '01', 'ea', '41', '00', 'e0', '6c']
             flag = 1
             print('0')
         else:
             flag = 0
-            r = ['6c', '00', 'e0', '6c', '6c', '00', 'e0', '6c', '6c', '00', 'e0', '6c', '6c', '01', '6c', '00', 'e0', '6c', '6c', '01', 'e0', '6c', '6c']
+            r = ['00', '1e', '6c', '01', 'ea', '44', '00', 'e2', '6c']
             print('1')
 #        a = json.dumps(r).encode('utf-8')
-        r.append('p001')
         a = pickle.dumps(r)
         try:
             s.send(a)
--- webserver/dogApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DogappConfig(AppConfig):
-    name = 'dogApp'
